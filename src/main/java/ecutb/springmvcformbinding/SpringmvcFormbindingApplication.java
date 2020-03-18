ef59a9a296813ce514f0edd2c055d1223f764f60
Oscar Johanneson <oscar.johanneson@gmail.com>
2020-03-18 12:14:44 +0100

    First commit

---
+++ src/main/java/ecutb/springmvcformbinding/SpringmvcFormbindingApplication.java
@@ -0,0 +1,13 @@
+package ecutb.springmvcformbinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcFormbindingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcFormbindingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ecutb/springmvcformbinding/SpringmvcFormbindingApplicationTests.java
@@ -0,0 +1,13 @@
+package ecutb.springmvcformbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcFormbindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
